8bbdd27e4bc45af6f3893e24253d01f5e751472d
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-07 09:35:34 +0300

    Added titles for pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,15 @@
+import Head from "next/head";
+
 const NotFound = () =>{
     return(
         <>
-            <h1>404 Ne nashel</h1>
-            <p>Oooooops.....</p>
+            <Head>
+                <title>404</title>
+            </Head>
+            <div>
+                <h1>404 Ne nashel</h1>
+                <p>Oooooops.....</p>
+            </div>
         </>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import {Layout} from "@/components/Layout";
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    useEffect(()=>{
+        document.getElementById('__next').classList.add('wrapper');
+        return ()=>{
+            document.getElementById('__next').classList.remove('wrapper')
+        }
+    },[])
+  return <Layout>
+            <Component {...pageProps} />
+          </Layout>
 }
