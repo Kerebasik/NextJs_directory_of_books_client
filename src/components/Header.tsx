adc9690b7a1386debde4a80158b439d2fb1dce02
Кирилл <kirill.denisenko.01@gmail.com>
2023-06-19 14:26:14 +0300

    Updated Header for app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,25 @@ import {FC} from "react";
 import Container from "./Container";
 import Link from "next/link";
 import styles from '@/styles/Header.module.scss'
+import Image from "next/image";
+import logo from '../../public/logo.png'
 
 const Header:FC=()=>{
     return(
+        <>
         <header className={styles.header}>
             <Container>
-                <nav className={styles.navigate}>
-                    <Link href={'/'}>Home</Link>
-                    <Link href={'/books/book/1'}>Catalog</Link>
-                    <Link href={'/books'}>Terms of use</Link>
-                </nav>
+                <div className={styles.content}>
+                    <Image src={logo} alt={'icon'} className={styles.logo} />
+                    <nav className={styles.navigate}>
+                        <Link href={'/'}>Home</Link>
+                        <Link href={'/books'}>Catalog</Link>
+                        <Link href={'/terms'}>Terms of use</Link>
+                    </nav>
+                </div>
             </Container>
         </header>
+            </>
     )
 }
 
