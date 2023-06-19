530f62833b610ced26864a79918b621f9c1927f7
Кирилл <kirill.denisenko.01@gmail.com>
2023-06-19 14:22:36 +0300

    Created Term of Use page

---
+++ src/components/pages/TermsOfUse.tsx
@@ -0,0 +1,97 @@
+import {FC} from "react";
+import styles from '@/styles/TermsOfUse.module.scss'
+
+export const TermsOfUse:FC = ()=>{
+    return(
+        <div className={styles.termsOfUse}>
+            <h1>Terms of use</h1>
+            <div className={styles.list}>
+                <ul>
+                    <li>
+                        Acceptance The Use Of Библиотека Terms and Conditions
+                        <ul>
+                            <li>Your access to and use of Библиотека is subject exclusively to these Terms and Conditions. You will not use the Website for any purpose that is unlawful or prohibited by these Terms and Conditions. By using the Website you are fully accepting the terms, conditions and disclaimers contained in this notice. If you do not accept these Terms and Conditions you must immediately stop using the Website.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Credit card details
+                        <ul>
+                            <li>Библиотека will never ask for Credit Card details and request that you do not enter it on any of the forms on Библиотека.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Advice
+                        <ul>
+                            <li>The contents of Библиотека website do not constitute advice and should not be relied upon in making or refraining from making, any decision.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Change of Use
+                        Библиотека reserves the right to:
+                        <ul>
+                            <li>
+                                change or remove (temporarily or permanently) the Website or any part of it without notice and you confirm that Библиотека shall not be liable to you for any such change or removal and.
+                            </li>
+                            <li>
+                                change these Terms and Conditions at any time, and your continued use of the Website following any changes shall be deemed to be your acceptance of such change.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Links to Third Party Websites
+                        <ul>
+                            <li>
+                                Библиотека Website may include links to third party websites that are controlled and maintained by others. Any link to other websites is not an endorsement of such websites and you acknowledge and agree that we are not responsible for the content or availability of any such sites.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Copyright
+                        <ul>
+                            <li>
+                                All copyright, trade marks and all other intellectual property rights in the Website and its content (including without limitation the Website design, text, graphics and all software and source codes connected with the Website) are owned by or licensed to Библиотека or otherwise used by Библиотека as permitted by law.
+                            </li>
+                            <li>
+                                In accessing the Website you agree that you will access the content solely for your personal, non-commercial use. None of the content may be downloaded, copied, reproduced, transmitted, stored, sold or distributed without the prior written consent of the copyright holder. This excludes the downloading, copying and/or printing of pages of the Website for personal, non-commercial home use only.
+
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Disclaimers and Limitation of Liability
+                        <ul>
+                            <li>
+                                The Website is provided on an AS IS and AS AVAILABLE basis without any representation or endorsement made and without warranty of any kind whether express or implied, including but not limited to the implied warranties of satisfactory quality, fitness for a particular purpose, non-infringement, compatibility, security and accuracy.
+                            </li>
+                            <li>
+                                To the extent permitted by law, Библиотека will not be liable for any indirect or consequential loss or damage whatever (including without limitation loss of business, opportunity, data, profits) arising out of or in connection with the use of the Website.
+                            </li>
+                            <li>
+                                Библиотека makes no warranty that the functionality of the Website will be uninterrupted or error free, that defects will be corrected or that the Website or the server that makes it available are free of viruses or anything else which may be harmful or destructive.
+                            </li>
+                            <li>
+                                Nothing in these Terms and Conditions shall be construed so as to exclude or limit the liability of Библиотека for death or personal injury as a result of the negligence of Библиотека or that of its employees or agents.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Indemnity
+                        <ul>
+                            <li>
+                                You agree to indemnify and hold Библиотека and its employees and agents harmless from and against all liabilities, legal fees, damages, losses, costs and other expenses in relation to any claims or actions brought against Библиотека arising out of any breach by you of these Terms and Conditions or other liabilities arising out of your use of this Website.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Severance
+                        <ul>
+                            <li>
+                                If any of these Terms and Conditions should be determined to be invalid, illegal or unenforceable for any reason by any court of competent jurisdiction then such Term or Condition shall be severed and the remaining Terms and Conditions shall survive and remain in full force and effect and continue to be binding and enforceable.
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
