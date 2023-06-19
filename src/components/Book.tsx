765508717b718ef9409efa94ef62ebb911b6eda8
Кирилл <kirill.denisenko.01@gmail.com>
2023-06-19 14:42:23 +0300

    Updated Book and BookList components

---
+++ src/components/Book.tsx
@@ -0,0 +1,39 @@
+import {IBook} from "@/models/IBook";
+import {FC, useEffect, useState} from "react";
+import styles from '@/styles/Book.module.scss'
+import imageStatic from '../../public/Book.png'
+import Image from "next/image";
+import {v4} from "uuid";
+import {getImage} from "@/http/getImage";
+
+interface BookProps{
+    item:IBook
+}
+
+export const Book:FC<BookProps>=({item})=>{
+    const [image, setImage] = useState<string>(imageStatic)
+
+    useEffect(()=>{
+        const reqImage = async ()=>{
+            if(!!item.image){
+                const imageFromAPI = await getImage(item.image)
+                setImage(imageFromAPI)
+            }
+        }
+        reqImage()
+    })
+
+    return(
+        <div className={styles.book}>
+            <div className={styles.image}>
+                <Image src={image} quality={100} fill alt={'icon'}/>
+            </div>
+            <div className={styles.text}>
+                <h3 className={styles.title}>{item.name}</h3>
+                <p className={styles.author}>{item.author}</p>
+                <p className={styles.tags}>{item.tags.length!==0 && <>{item.tags.map(item=> <font key={`${v4}.${item}`}>{item}, </font>)}</>}</p>
+                <p className={styles.description}></p>
+            </div>
+        </div>
+    );
+}
--- src/components/BooksList.tsx
+++ src/components/BooksList.tsx
@@ -1,10 +1,27 @@
-
 import {FC} from "react";
+import {IBook} from "@/models/IBook";
+import {Book} from "@/components/Book";
+import styles from '@/styles/BooksList.module.scss'
+import Link from "next/link";
 
-const BooksList:FC =()=>{
+interface BookListProps{
+    data:IBook[]
+}
+
+const BooksList:FC<BookListProps>=({data})=>{
     return(
         <>
-
+            <div className={styles.panel}>
+                <Link href={'/books/book/create'}><button>Create</button></Link>
+            </div>
+            <div className={styles.list}>
+                { data.map((item:IBook)=>(
+                        <Link key={item._id} href={'/books/book/'+item._id}>
+                            <Book key={item._id} item={item}/>
+                        </Link>
+                    )
+                )}
+            </div>
         </>
     )
 }
